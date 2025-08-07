c2a4f8004cd410322f47751baf9e7e975d0bb3b5
Kavia AI <production@kavia.ai>
2025-08-07 17:19:21 +0000

    CheckPoint - cg32c5cc2e

---
+++ micro_frontend_dashboard/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: 'auto',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard_host',
+      remotes: {
+        assets: 'assets@http://localhost:3001/remoteEntry.js',
+        explorer: 'explorer@http://localhost:3002/remoteEntry.js',
+        templates: 'templates@http://localhost:3003/remoteEntry.js',
+      },
+      shared: { 
+        react: { singleton: true, requiredVersion: '^18.2.0' },
+        "react-dom": { singleton: true, requiredVersion: '^18.2.0' },
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ]
+};
